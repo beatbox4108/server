c3815e7c9f99e4e45d31dca7297d80d5d9119692
beatbox4108 <airpocket.100c@gmail.com>
2021-05-18 19:41:43 +0900

    更新

--- docs/media/js/content.js
+++ docs/media/js/content.js
@@ -1,6 +1,6 @@
 function load_header(){
-    fetch("//www.beatbox4108.tk/media/temprate/footer.temprate").then((val)=>{
-        document.querySelector(".content-js[data-content-type=\"footer\"]").innerHTML=val.text()
+    fetch("//www.beatbox4108.tk/media/temprate/footer.temprate").then((val)=>val.text()).then((val)=>{
+        document.querySelector(".content-js[data-content-type=\"footer\"]").innerHTML=val
     }).catch(()=>{
         document.querySelector(".content-js[data-content-type=\"footer\"]").innerHTML="<div class=\"footer-error\">Sorry, loading failed. Please <a href=\"#\",onclick=\"load_header()\">reload</a></div>"
     });
