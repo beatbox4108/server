38f760167943feabedde172bfdfe78b6947351d1
beatbox4108 <airpocket.100c@gmail.com>
2021-05-18 19:14:40 +0900

    反映されていなかったので修正

--- docs/media/js/content.js
+++ docs/media/js/content.js
@@ -1,12 +1,12 @@
 function load_header(){
-    fetch("https://www.beatbox4108.tk/media/temprate/footer.temprate").then((val)=>{
+    fetch("https://beatbox4108.tk/media/temprate/footer.temprate").then((val)=>{
         document.querySelector(".content-js[data-content-type=\"footer\"]").innerHTML=val.text
     }).catch(()=>{
-        fetch("http://www.beatbox4108.tk/media/temprate/footer.temprate").then((val)=>{
+        fetch("https://beatbox4108.tk/media/temprate/footer.temprate").then((val)=>{
             document.querySelector(".content-js[data-content-type=\"footer\"]").innerHTML=val.text
         }).catch(()=>{
             document.querySelector(".content-js[data-content-type=\"footer\"]").innerHTML="<div class=\"footer-error\">Sorry, loading failed. Please <a href=\"#\",onclick=\"load_header()\">reload</a></div>"
-    })
+        })
     })
 }
 document.addEventListener("DOMContentLoaded",()=>{
