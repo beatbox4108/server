bd769cbb085b7edb226b3819e96faf6dd95dc891
beatbox4108 <airpocket.100c@gmail.com>
2021-06-13 19:42:23 +0900

    編集ミス

--- docs/media/js/content.js
+++ docs/media/js/content.js
@@ -2,7 +2,7 @@ function load_header(){
     document.querySelector(".content-js[data-content-type=\"footer\"]").innerHTML=`
 <section>
     <section>
-    <a href="https://twitter.com/intent/tweet?url=This is so good! ${location.href}"><img src="//www.beatbox4108.tk/media/img/sns-icon/twitter.png" width="30",height="30"></a>
+    <a href="https://twitter.com/intent/tweet?url=This is so good! ${location.href}"><img src="//www.beatbox4108.tk/media/img/sns-icon/twitter.png" width="30"></a>
     <a href="https://b.hatena.ne.jp/entry/" class="hatena-bookmark-button" data-hatena-bookmark-layout="vertical-normal" data-hatena-bookmark-lang="en" title="このエントリーをはてなブックマークに追加"><img src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a><script type="text/javascript" src="https://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>
         <iframe src="https://www.facebook.com/plugins/like.php?href=${location.href}&width=92&layout=box_count&action=like&size=small&share=true&height=65&appId" width="92" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"></iframe>
     </section>
