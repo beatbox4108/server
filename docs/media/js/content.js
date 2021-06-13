f4c4d1ddc11c2155286a075b135d9bce5c54c638
beatbox4108 <airpocket.100c@gmail.com>
2021-06-13 14:21:03 +0900

    fix error

--- docs/media/js/content.js
+++ docs/media/js/content.js
@@ -1,5 +1,5 @@
 function load_header(){
-    fetch("//www.beatbox4108.tk/media/temprate/footer.temprate").then((val)=>val.text().replace("$url",location.href)).then((val)=>{
+    fetch("//www.beatbox4108.tk/media/temprate/footer.temprate", {mode: 'cors',credentials: 'include'}).then((val)=>val.text().replace("$url",location.href)).then((val)=>{
         document.querySelector(".content-js[data-content-type=\"footer\"]").innerHTML=val
     }).catch(()=>{
         document.querySelector(".content-js[data-content-type=\"footer\"]").innerHTML="<div class=\"footer-error\">Sorry, loading failed. Please <a href=\"#\",onclick=\"load_header()\">reload</a></div>"
